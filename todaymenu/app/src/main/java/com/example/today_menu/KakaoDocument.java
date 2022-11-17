ee81fe981d42f3c49d9e961f44cb4b5d8484fbe5
김보미 <kimbomi2172@naver.com>
2022-11-17 16:48:18 +0900

    구글맵

---
+++ todaymenu/app/src/main/java/com/example/today_menu/KakaoDocument.java
@@ -0,0 +1,15 @@
+package com.example.today_menu;
+
+import java.util.ArrayList;
+
+public class KakaoDocument {
+    private ArrayList<KakaoLocation> documents;
+
+    public ArrayList<KakaoLocation> getDocuments() {
+        return documents;
+    }
+
+    public void setDocuments(ArrayList<KakaoLocation> documents) {
+        this.documents = documents;
+    }
+}
---
+++ todaymenu/app/src/main/java/com/example/today_menu/MenuResultActivity.java
@@ -0,0 +1,7 @@
+package com.example.today_menu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MenuResultActivity extends AppCompatActivity {
+
+}
