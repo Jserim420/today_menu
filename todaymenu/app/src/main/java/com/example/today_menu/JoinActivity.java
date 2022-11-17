f3af5f5de8eaaa7a6e2e2ee05da3b218ab7a24ad
DESKTOP-HI8UGCA\USER <wkdtpfla420@gmail.com>
2022-11-17 18:35:37 +0900

    구글맵, firebase auth 추가

--- todaymenu/app/src/main/java/com/example/today_menu/JoinActivity.java
+++ todaymenu/app/src/main/java/com/example/today_menu/JoinActivity.java
@@ -3,12 +3,21 @@ package com.example.today_menu;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class JoinActivity extends AppCompatActivity {
 
@@ -24,6 +33,7 @@ public class JoinActivity extends AppCompatActivity {
 
         EditText emailEt=findViewById(R.id.email_et);
         EditText passwordEt=findViewById(R.id.password_et);
+        EditText passwordConfirmEt=findViewById(R.id.password_confirm_et);
         Button joinBtn = findViewById(R.id.join_btn);
 
 
@@ -32,13 +42,45 @@ public class JoinActivity extends AppCompatActivity {
             public void onClick(View view) {
                 String email = emailEt.getText().toString();
                 String password = passwordEt.getText().toString();
+                String passwordConfirm = passwordConfirmEt.getText().toString();
 
-                Intent intent = new Intent(JoinActivity.this, LoginActivity.class);
+                if(email.equals("이메일을 입력해주세요.")) {
+                    Toast.makeText(JoinActivity.this, "이메일을 입력해 주세요.", Toast.LENGTH_LONG).show();
+                }
+                if(!email.contains("@")) {
+                    Toast.makeText(JoinActivity.this, "올바른 일메일 방식으로 입력해 주세요.", Toast.LENGTH_LONG).show();
+                }
+                if(password.equals("비밀번호를 입력해주세요.")) {
+                    Toast.makeText(JoinActivity.this, "비밀번호를 입력해 주세요.", Toast.LENGTH_LONG).show();
+                }
+                if(!password.equals(passwordConfirm)) {
+                    Toast.makeText(JoinActivity.this, "비밀번호가 일치하지 않습니다.", Toast.LENGTH_LONG).show();
+                }
 
-                intent.putExtra("email", email);
-                intent.putExtra("password", password);
+                FirebaseAuth auth = FirebaseAuth.getInstance();
+                auth.createUserWithEmailAndPassword(email,password)
+                        .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                            @Override
+                            public void onSuccess(AuthResult authResult) {
+                                FirebaseUser user = authResult.getUser();
 
-                startActivity(intent);
+                                Intent intent = new Intent(JoinActivity.this, LoginActivity.class);
+
+                                intent.putExtra("email", email);
+                                intent.putExtra("password", password);
+
+                                startActivity(intent);
+
+                                Toast.makeText(JoinActivity.this, "회원가입이 완료되었습니다.", Toast.LENGTH_LONG).show();
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.d("JoinActiviy", e.getMessage());
+                                Toast.makeText(JoinActivity.this, "회원가입에 실패하였습니다.", Toast.LENGTH_LONG).show();
+                            }
+                        });
             }
         });
     }
--- todaymenu/app/src/main/java/com/example/today_menu/LoginActivity.java
+++ todaymenu/app/src/main/java/com/example/today_menu/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.today_menu;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,6 +9,13 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -15,8 +23,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Log.d("MainActivity","onCreate");
 
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+
+        // 회원가입->로그인 정보 불러오기
         Intent intent1 = getIntent();
         String email = intent1.getStringExtra("email");
         String password = intent1.getStringExtra("password");
@@ -41,9 +51,36 @@ public class LoginActivity extends AppCompatActivity {
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(intent);
+
+                String email = emailEt.getText().toString();
+                String password = passwordEt.getText().toString();
+
+                auth.signInWithEmailAndPassword(email,password)
+                        .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                            @Override
+                            public void onSuccess(AuthResult authResult) {
+                                completeLogin();
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.d("LoginActivity",e.getMessage());
+                                Toast.makeText(LoginActivity.this, "아이디 혹은 비밀번호가 틀렸습니다.", Toast.LENGTH_LONG).show();
+                            }
+                        });
             }
         });
     }
+
+    public void completeLogin() {
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        FirebaseUser user = auth.getCurrentUser();
+        if(user!=null) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+
 }
