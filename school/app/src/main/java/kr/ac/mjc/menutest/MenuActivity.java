a041b6913a8bc6ff5e0aa4a95d01759066b390e1
YDA0 <dayoung9746@gmail.com>
2022-11-17 11:10:09 +0900

    test

--- school/app/src/main/java/kr/ac/mjc/menutest/MenuActivity.java
+++ school/app/src/main/java/kr/ac/mjc/menutest/MenuActivity.java
@@ -1,6 +1,9 @@
 package kr.ac.mjc.menutest;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,6 +13,22 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mainq_main);
+
+        Button koreanBtn = findViewById(R.id.korean_btn);
+
+        koreanBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MenuActivity.this, KoreanActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
+
+
 }
+
+
+
--- school/app/src/main/java/kr/ac/mjc/menutest/WesternActivity.java
+++ school/app/src/main/java/kr/ac/mjc/menutest/WesternActivity.java
@@ -4,10 +4,11 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class KoreanActivity extends AppCompatActivity {
+public class WesternActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.korean_main);
+        setContentView(R.layout.foodclass_main);
     }
 }
