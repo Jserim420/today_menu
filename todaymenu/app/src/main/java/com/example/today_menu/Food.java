8a18ee7d1a205b3d5e3b1dfe252f6f383e1ef933
DESKTOP-HI8UGCA\USER <wkdtpfla420@gmail.com>
2022-11-22 12:04:33 +0900

    firestore update

---
+++ todaymenu/app/src/main/java/com/example/today_menu/Food.java
@@ -0,0 +1,49 @@
+package com.example.today_menu;
+
+public class Food {
+    private String name;
+    private int price;
+    private String country;
+    private String categoryFood;
+    private String taste;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCategoryFood() {
+        return categoryFood;
+    }
+
+    public void setCategoryFood(String categoryFood) {
+        this.categoryFood = categoryFood;
+    }
+
+    public String getTaste() {
+        return taste;
+    }
+
+    public void setTaste(String taste) {
+        this.taste = taste;
+    }
+}
